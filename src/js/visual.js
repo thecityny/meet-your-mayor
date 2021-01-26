24648f8a607883103eb4d9005980cbd56bb4dd08
Will Welch <wwelch@thecity.nyc>
2021-01-26 11:00:16 -0500

    Canvas conversion

--- src/js/visual.js
+++ src/js/visual.js
@@ -1,37 +1,41 @@
-const {select, namespaces} = require("d3-selection");
+const {select} = require("d3-selection");
 // const {scaleLinear} = require("d3-scale");
 // const {range} = require("d3-array");
 
 module.exports = function (target) {
   var updateFunction;
 
-  const svg = select(document.createElementNS(namespaces.svg, "svg"))
+  const canvas = select(document.createElement("canvas"))
     .attr("width", 0)
     .attr("height", 0);
+  const context = canvas.node().getContext("2d");
 
-  const circleParent = svg.append("g");
-
-  target.appendChild(svg.node());
+  target.appendChild(canvas.node());
 
   function join(candidates) {
-    const circles = circleParent.selectAll("circle")
-      .data(candidates)
-      .join("circle");
-
     function update() {
       const width = target.offsetWidth;
       const height = target.offsetHeight;
-      // const circleRadius = (width / candidates.length) / 2;
-      circleRadius = 10;
 
-      svg
+      canvas
         .attr("width", width)
         .attr("height", height);
 
-      circles
-        .attr("cx", (d, i) => i * circleRadius * 2 + circleRadius)
-        .attr("cy", height / 2)
-        .attr("r", circleRadius);
+      context.clearRect(0, 0, width, height);
+      context.save();
+      candidates.forEach((_, i) => {
+        // const r = (width / candidates.length) / 2;
+        const r = 10;
+        const x = i * r * 2 + r;
+        const y = height / 2;
+        
+        context.beginPath();
+        context.moveTo(x, y);
+        context.arc(x, y, r, 0, 2 * Math.PI);
+        context.fillStyle = "#000";
+        context.fill();
+      });
+      context.restore();
     }
 
     window.removeEventListener("resize", updateFunction);
